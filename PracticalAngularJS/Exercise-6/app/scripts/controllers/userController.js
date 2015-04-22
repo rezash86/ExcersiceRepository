8727108786c6a39adff2a4a024c8826ed053d780
Wei Ru <chris.ru@gmail.com>
2015-04-22 10:28:38 -0500

    Display users in UI Grid view

--- PracticalAngularJS/Exercise-6/app/scripts/controllers/userController.js
+++ PracticalAngularJS/Exercise-6/app/scripts/controllers/userController.js
@@ -5,43 +5,26 @@ myApp.controller('UserListCtrl', function($scope, UserService) {
     $scope.gridOptions = { 
         enableRowSelection: true,
         enableSelectAll: true,
-        selectionRowHeaderWidth: 35
+        selectionRowHeaderWidth: 35,
+        showColumnFooter: true
       };
 
     $scope.gridOptions.columnDefs = [
         { name: 'name', displayName: 'Name'},
-        { name: 'email', displayName: 'Email'},
+        { name: 'email', displayName: 'Email',
+          footerCellTemplate: '<div class="ui-grid-bottom-panel" style="text-align: center">Total Rows : {{grid.appScope.gridOptions.data.length}}</div>'    
+        },
         { name: 'group', displayName: 'Group' }
       ];
 
     $scope.gridOptions.multiSelect = true;
 
+    /**
     $scope.gridOptions.data = [
       { name: 'Bob', email: 'CEO@corp.com', group: false},
       { name: 'Frank', email: 'Lowly.Developer@corp.com', group: false}
     ];
-    
-    //$scope.gridOptions = {
-        //rowHeight:70,
-        //enableRowSelection: true, 
-        //enableRowHeaderSelection: true,
-        //selectionRowHeaderWidth: 35,
-        //enableSelectAll: true,
-        //multiSelect: false,
-        //noUnselect: false,
-        //showColumnFooter: true,
-        //enableSorting: true,
-        //enableCellEditOnFocus: true,
-        //columnDefs: [
-        //    { name: ' ' },
-         //   { field: 'name' }, 
-         //   { field: 'email' }, 
-         //   { field: 'group' }],
-     //   data: 
-        //onRegisterApi: function(gridApi) {
-        //  $scope.gridApi = gridApi;
-        //}
-    //};
+    **/
     
     $scope.listUsers = function() {
 		UserService.listUsers().then(function(data) {
